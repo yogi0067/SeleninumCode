90d4754aebf1a95403e94dd99b3167bc91c2b2fe
Yogesh Kumar (IND) <YogeshK@dev.oati.local>
2019-11-27 20:15:17 -0600

    update driver path

--- venv/orbitz/links.py
+++ venv/orbitz/links.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.keys import Keys
 
 
 #open the browser with
-driver = webdriver.Chrome(r"D:\Learning\Python\Automation\selenium\Project\seleniumTest\venv\orbitz\tools\chromedriver.exe")
+driver = webdriver.Chrome(r"venv\orbitz\tools\chromedriver.exe")
 driver.get('https://www.orbitz.com/')
 home = driver.find_element_by_xpath('//*[@id="primary-header-home"]')
 hotels = driver.find_element_by_xpath('//*[@id="primary-header-hotel"]')
